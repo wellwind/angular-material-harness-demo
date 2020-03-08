6025687aed86effaff297f010979f8a947b38e22
Mike <wellwind@mail2000.com.tw>
2020-03-08 15:18:40 +0800

    減少無用的程式碼

--- src/app/buttons-area/buttons-area.component.ts
+++ src/app/buttons-area/buttons-area.component.ts
@@ -1,19 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-buttons-area',
   templateUrl: './buttons-area.component.html',
   styleUrls: ['./buttons-area.component.css']
 })
-export class ButtonsAreaComponent implements OnInit {
+export class ButtonsAreaComponent {
   @Input() actions = ['Save', 'Exit'];
   @Input() isDisabled = false;
   @Output() actionSelected = new EventEmitter<string>();
   selectedAction: '';
-
-  constructor() {}
-
-  ngOnInit() {}
 
   triggerAction() {
     this.actionSelected.emit(this.selectedAction);
